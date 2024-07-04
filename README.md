bad18156dc82b0eeed5e313213e0a52189de444b
WhiteSnacc <albima.rasya66@gmail.com>
2024-07-04 19:43:11 +0700

    fix js

---
+++ README.md
@@ -0,0 +1,2 @@
+Mini project web for RevoU
+Website Link : https://revou-fundamental-course.github.io/fundamental-AlbimaRasya/
---
+++ js/script.js
@@ -0,0 +1,63 @@
+// Input nama user ke web
+let displayName = prompt('Ini siapa yh?','');
+document.getElementById("displayname").innerHTML= displayName;
+console.log("displayName")
+
+// Fungsi Banner AutoSlide
+// var slideIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("slide");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
+// let slideIndex = 0;
+// const slides = document.querySelectorAll('.slide');
+
+// function showSlides() {
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = 'none';
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {
+//         slideIndex = 1;
+//     }
+//     slides[slideIndex - 1].style.display = 'block';
+//     setTimeout(showSlides, 5000); // Change image every 2 seconds
+// }
+
+// showSlides();
+
+// Fungsi Output Input Form Message
+function setSenderUI(name, birthDate, gender, message) {
+    document.getElementById("sender-name").innerHTML = name;
+    document.getElementById("sender-birthdate").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-message").innerHTML = message;
+}
+
+function validateForm() {
+    const name = document.forms["message-form"]["name"].value;
+    const birthDate = document.forms["message-form"]["birthdate"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const message = document.forms["message-form"]["message"].value;
+
+    if (name != "" && birthDate != "" && gender != "" && message != "") {
+        alert("Form terkirim");
+        setSenderUI(name, birthDate, gender, message);
+        return false
+    }
+    else {
+        alert("Tidak boleh kosong!");
+        return false;
+    }
+}
+
+console.log("Hello world");
