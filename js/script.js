52c602e284216985feea26dc8ed2d60aae54ba57
WhiteSnacc <albima.rasya66@gmail.com>
2024-07-04 22:47:13 +0700

    Full-rilis

--- js/script.js
+++ js/script.js
@@ -10,36 +10,34 @@ function replaceName() {
 
 
 // Fungsi Banner AutoSlide
-// var slideIndex = 0;
-// carousel();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// function carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("slide");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > x.length) {slideIndex = 1}
-//   x[slideIndex-1].style.display = "block";
-//   setTimeout(carousel, 2000); // Change image every 2 seconds
-// }
-// let slideIndex = 0;
-// const slides = document.querySelectorAll('.slide');
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-// function showSlides() {
-//     for (let i = 0; i < slides.length; i++) {
-//         slides[i].style.display = 'none';
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {
-//         slideIndex = 1;
-//     }
-//     slides[slideIndex - 1].style.display = 'block';
-//     setTimeout(showSlides, 5000); // Change image every 2 seconds
-// }
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-// showSlides();
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 // Fungsi Output Input Form Message
 function setSenderUI(name, birthDate, gender, message) {
